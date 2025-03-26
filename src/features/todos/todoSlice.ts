721677322872a9bb8db9142ae502f0993c2d3fb6
Jigneshsingh-growexxer <jigneshsingh.kushvaha@growexx.com>
2025-03-26 19:52:33 +0530

    remove unnecessary imports

--- src/features/todos/todoSlice.ts
+++ src/features/todos/todoSlice.ts
@@ -1,5 +1,5 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { Todo, TodoState } from './todoTypes';
+import { TodoState } from './todoTypes';
 import {
   fetchTodos,
   addTodoAsync,
